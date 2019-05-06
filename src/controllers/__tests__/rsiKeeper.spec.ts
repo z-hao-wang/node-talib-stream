c171ee4038493424fa532dabc9a741c6e56eedb4
wanghao <whateverhow@gmail.com>
2019-05-06 00:39:13 -0700

    [master] 	modified:   src/controllers/__tests__/rsiKeeper.spec.ts

--- src/controllers/__tests__/rsiKeeper.spec.ts
+++ src/controllers/__tests__/rsiKeeper.spec.ts
@@ -23,8 +23,6 @@ describe('rsiKeeper', () => {
       optInTimePeriod: period,
     });
 
-    // for some reason, talib initial results are calculated differently with our approach
-    // but over time, the values are trending to equal
     _.each(talibRes.result.outReal, (t, i: number) => {
       expect(Math.abs(t - rsiKeeperRes[i + period]) < 0.00001).toBeTruthy();
     });
